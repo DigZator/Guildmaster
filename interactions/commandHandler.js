8da261d1f1f3f5aa94e6ce2b250c2e5be88aeea1
DigZator <omsbhise@gmail.com>
2026-04-25 14:24:20 +0530

    Corrected type

--- interactions/commandHandler.js
+++ interactions/commandHandler.js
@@ -1,4 +1,4 @@
-const { gameAutocomplete } = require('../utils/gameAutoComplete');
+const { gameAutoComplete } = require('../utils/gameAutoComplete');
 
 module.exports = (client) => {
     client.on('interactionCreate', async (interaction) => {
